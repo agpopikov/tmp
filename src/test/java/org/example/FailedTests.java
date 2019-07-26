8c768bd56fbbb1d984fb65d778a3ec80cee4c4e1
agpopikov <agpopikov@gmail.com>
2019-07-26 16:04:02 +0300

    SOmething

---
+++ src/test/java/org/example/FailedTests.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FailedTests {
+
+    @Test
+    public void exampleTest() {
+        Assert.fail();
+    }
+}
